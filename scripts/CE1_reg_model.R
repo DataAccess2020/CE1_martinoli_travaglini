3de62d71adda02fbb58a91a67707ad7b7cb77555
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-11-26 19:29:40 +0100

    i add the comment

--- scripts/CE1_reg_model.R
+++ scripts/CE1_reg_model.R
@@ -3,3 +3,5 @@
 regression <- lm(jst_eut ~ imp_rel, data = dat_pro)
 summary(regression)
 #the output its a bit confused: if you look the probability you can see a weird format, can you fix it?
+
+# intercept = 8.3, coefficient = -1.2, adjusted R-squared = 0.141, *** significance
