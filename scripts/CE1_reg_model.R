4ba16640d0d945a1cd8c9f765d9e53c535bfbd84
simonetravaglini1 <simonetravaglini@studenti.unimi.it>
2022-11-27 19:39:47 +0100

    i edited the title

--- scripts/CE1_reg_model.R
+++ scripts/CE1_reg_model.R
@@ -1,9 +1,12 @@
-# linear regression model
+# linear regression model------
 
 options(scipen = 99)
 
 regression <- lm(jst_eut ~ imp_rel, data = dat_pro)
 summary(regression)
 
-
-# intercept = 8.3, coefficient = -1.2, adjusted R-squared = 0.141,  *** significance
+# statistical data--------
+# intercept = 8.3
+#coefficient = -1.2
+#adjusted R-squared = 0.141 
+# *** significance
